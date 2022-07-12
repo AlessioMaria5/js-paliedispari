e5a1996059506d5b8a486836a07a47f4fbd69d74
AlessioMaria5 <alessiomaria5@gmail.com>
2022-07-12 14:25:42 +0200

    step 1 to 2.1

---
+++ main.js
@@ -0,0 +1,20 @@
+// PALIDROMA
+// 1- Chiedere all'utente di inserire una parola [ Y ]
+// 2 -Creare una funzione per capire se la parola inserita è un palindromo
+// 2.1 = scomporre la parola [y]
+// 2.2 fargli leggere la parola al contrario
+// 2.3 se la parola al contrario è uguale alla parola letta normale allora è palindroma.
+
+
+
+let parolaInserita = prompt('Inserisci una parola');
+
+console.log(parolaInserita);
+
+function palindroma () {
+
+    const parolaScomposta = parolaInserita.split('');
+    console.log(parolaScomposta);
+
+}
+
