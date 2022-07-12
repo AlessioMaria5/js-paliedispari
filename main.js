48fd55f241e1d57c302607e86771a2c2a4cadfac
AlessioMaria5 <alessiomaria5@gmail.com>
2022-07-12 14:35:04 +0200

    parola al contrario

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
 // 1- Chiedere all'utente di inserire una parola [ Y ]
 // 2 -Creare una funzione per capire se la parola inserita è un palindromo
 // 2.1 = scomporre la parola [y]
-// 2.2 fargli leggere la parola al contrario
+// 2.2 fargli leggere la parola al contrario [y]
 // 2.3 se la parola al contrario è uguale alla parola letta normale allora è palindroma.
 
 
@@ -11,7 +11,11 @@ let parolaInserita = prompt('Inserisci una parola');
 console.log(parolaInserita);
 const parolaScomposta = parolaInserita.split('');
 console.log(parolaScomposta);
+const parolaScompostaContrario = parolaScomposta.reverse('');
+console.log(parolaScompostaContrario);
 
 function palindroma () {
+
+
 
 }
