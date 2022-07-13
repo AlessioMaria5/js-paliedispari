e085aba20a2ea4cacf09be5e18a4635d784507bf
AlessioMaria5 <alessiomaria5@gmail.com>
2022-07-13 09:21:02 +0200

    refactoring e correzione bug

--- main.js
+++ main.js
@@ -1,94 +1,23 @@
-// PALIDROMA
-// 1- Chiedere all'utente di inserire una parola [ Y ]
-// 2 -Creare una funzione per capire se la parola inserita è un palindromo
-// 2.1 = scomporre la parola [y]
-// 2.2 fargli leggere la parola al contrario
-// 2.3 se la parola al contrario è uguale alla parola letta normale allora è palindroma.
-
-
-
 let parolaInserita = prompt('Inserisci una parola');
-const parolaScomposta = parolaInserita.split('');
 
-
-
-function parolaAlContrario(){ 
+function parolaAlContrario(parola){ 
 
     let x = 0 
 
-    for( i = parolaScomposta.length -1 ; i >= 0; i--) {
-
-        if(parolaScomposta[i] == parolaScomposta[x]){
+    for( i = parola.length -1 ; i >= 0; i--) {
+        if(parola[i] != parola[x]){
             return false
         }
     x++ 
-
-    return true;
     }
 }
 
-let finale = parolaAlContrario();
+let finale = parolaAlContrario(parolaInserita);
 
-if (finale == true) {
-
+if (finale == false) {
     alert('normale')
 }
 
-else if (finale == false) {
-
+else {
     alert('palindromo');
 }
-
-
-
-
-
-// const parolaScompostaContrario = parolaScomposta.reverse('');  -- < METODO REVERSE >
-// console.log(parolaScompostaContrario); -- < METODO REVERSE > --------------------------------
-
-
-// function dxTo(){
-
-//     for( i = parolaScomposta.length -1 ; i >= 0; i--) {
-
-//         console.log('il numero del ciclo è ' + i);
-//         console.log(parolaScomposta[i]);
-//     }
-
-//     alert(parolaScomposta);
-
-    
-// }
-
-// function sxTo(){
-
-//     for( i = 0; i < parolaScomposta.length; i++) {
-
-//         console.log('il numero del ciclo è ' +i);
-//         console.log(parolaScomposta);
-
-//     }
-
-    
-
-// }   
-
-
-
-// function palindroma() {
-
-//     for( i = parolaScomposta.length; i >= 0; i--) {
-
-//         console.log('il numero del ciclo è ' + i);
-//         const risultato = console.log(parolaScomposta[i]);
-    
-//     }
-
-//     for( i = 0; i < parolaScomposta.length; i++) {
-
-
-//     }
-
-
-// }
-
