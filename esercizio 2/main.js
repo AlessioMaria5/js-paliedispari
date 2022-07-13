cc2f8f16cffab8ea868c40e890beb10212be963b
AlessioMaria5 <alessiomaria5@gmail.com>
2022-07-13 10:37:28 +0200

    da inserire condizione scelta pari o dispari

---
+++ esercizio 2/main.js
@@ -0,0 +1,31 @@
+// Pari e Dispari
+// L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
+// Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
+// Sommiamo i due numeri
+// Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
+// Dichiariamo chi ha vinto.
+
+// let pariDispari = prompt('Inserisci Pari o Dispari');
+let inserisciNumero = prompt('Inserisci Numero da 1 a 5');
+
+function sommaSfida (x) {
+    let computer = Math.floor(Math.random() * 5 +1);
+    let numeroInserito = parseInt(x);
+    let risultato = parseInt(computer + numeroInserito)
+
+    return risultato;
+}
+
+let finale = sommaSfida(inserisciNumero);
+console.log(finale);
+
+if(finale % 2 == 0) {
+
+    alert('vince il computer!');
+}
+
+else {
+
+    alert('vinci tu');
+}
+
